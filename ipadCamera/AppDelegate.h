ae9559316d8bb35166b61e6db7f957ebda174666
chenkaiios <chenyongkaiios@163.com>
2017-08-01 15:29:23 +0800

    iPad调用相机

---
+++ ipadCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ipadCamera
+//
+//  Created by NewBest_RD on 2017/6/13.
+//  Copyright © 2017年 newbest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ipadCamera/ViewController.m
@@ -0,0 +1,56 @@
+//
+//  ViewController.m
+//  ipadCamera
+//
+//  Created by NewBest_RD on 2017/6/13.
+//  Copyright © 2017年 newbest. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor=[UIColor blueColor];
+    
+    UIImagePickerController *pickerImage = [[UIImagePickerController alloc] init];
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        pickerImage.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        //pickerImage.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+        pickerImage.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:pickerImage.sourceType];
+        
+    }
+    pickerImage.delegate = self;
+    pickerImage.allowsEditing = NO;
+    [self presentModalViewController:pickerImage animated:YES];
+    
+}
+
+
+#pragma mark - imagePickerControllerDelegate
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+
+    
+}
+
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(nullable NSDictionary<NSString *,id> *)editingInfo {
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
